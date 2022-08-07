fa05641893024b70f472f1db94f57e803324bb5f
Victoriya <victory_alex@mail.ru>
2022-08-07 16:42:47 +0300

    Поиск индекса числа n

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = {1, 26, 56, 35, 34, 56, 6, 76, 8, 9};
+
+int n = array.Length;
+
+int find = 56;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
